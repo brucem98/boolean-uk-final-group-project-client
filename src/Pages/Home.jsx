35667c82d737dbfbc3f62fdbee220732baf99069
unaben <unaben@yahoo.com>
2021-11-11 18:11:36 +0000

    none

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,45 +1,46 @@
-
-import {useNavigate}  from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function Home(props) {
-    const navigate = useNavigate();
-    function handleClick (event, competition) {
-        event.preventDefault()
-        navigate(`/competition/${competition.id}`)
-    }
-    const competitions = props.competitions.map((competition, index) => {
-        return (
-            <li key={index}>
-                <h3>{competition.exhibitionName}</h3>
-                <p>Date: {competition.date}</p>
-                <p>Location: {competition.location}</p>
-                <p>Difficulty: {competition.difficulty}</p>
-                <p>Prize Pool: {competition.prizePool}</p>
-                <p>Capacity: {competition.capacity}</p>
-                <img src={competition.img} alt="Competition" />
-                <button onClick = {(event)=>handleClick(event, competition)}>More Details</button>
-            </li>
-        )
-    })
+  const navigate = useNavigate();
 
+  function handleClick(event, competition) {
+    event.preventDefault();
+    navigate(`/competition/${competition.id}`);
+  }
+
+  const competitions = props.competitions.map((competition, index) => {
     return (
-        <>
-            <header>
-                <h1>Dog Shows</h1>
-            </header>
-            <main>
-                <section>
-                    <h2>List of all upcoming dog competitions:</h2>
-                    <p>*FILTER COMPETITION BY DIFFICULTY HERE*</p>
-                </section>
-                <section>
-                    <ul>
-                        {competitions}
-                    </ul>
-                </section>
-            </main>
-        </>
-    )
+      <li key={index}>
+        <h3>{competition.exhibitionName}</h3>
+        <p>Date: {competition.date}</p>
+        <p>Location: {competition.location}</p>
+        <p>Difficulty: {competition.difficulty}</p>
+        <p>Prize Pool: {competition.prizePool}</p>
+        <p>Capacity: {competition.capacity}</p>
+        <img src={competition.img} alt="Competition" />
+        <button onClick={(event) => handleClick(event, competition)}>
+          More Details
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <header>
+        <h1>Dog Shows</h1>
+      </header>
+      <main>
+        <section>
+          <h2>List of all upcoming dog competitions:</h2>
+          <p>*FILTER COMPETITION BY DIFFICULTY HERE*</p>
+        </section>
+        <section>
+          <ul>{competitions}</ul>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default Home
+export default Home;
