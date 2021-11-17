248482bf1d53397936896e9042f1e46af004ca96
unaben <unaben@yahoo.com>
2021-11-17 16:46:44 +0000

    created_Registration_form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router-dom";
 
 function Home(props) {
-    const navigate = useNavigate();
-    function handleClick (event, competition) {
-        event.preventDefault()
-        navigate(`/competitions/${competition.id}`)
-    }
-    
+  const navigate = useNavigate();
+
+  function handleClick(event, competition) {
+    event.preventDefault();
+
+    navigate(`/competitions/${competition.id}`);
+  }
+
   const competitions = props.competitions.map((competition, index) => {
     return (
       <li key={index}>
--- src/Pages/SingleTicket.jsx
+++ src/Pages/SingleTicket.jsx
@@ -2,12 +2,12 @@ function SingleTicket(props) {
     const {firstName, lastName, vaccinated, competition, id } = props.ticket
 console.log("props in singleticket: ", props)
 
-    const handleTicketDelete = (event) => {
+    const handleTicketDelete = () => {
 
         const fetchTicketToDelete = {
             method: "DELETE",
         };
-        const delUrl = `http://localhost:3030/tickets/${id}`
+        const delUrl =`${process.env.REACT_APP_FETCH_URL}/tickets${id}`
         
         console.log("Inside ticket delUrl: ", delUrl)
 
