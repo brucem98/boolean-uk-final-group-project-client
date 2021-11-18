fc939ccb318d9cf4e12f6f5fde0f52bbae20903b
unaben <unaben@yahoo.com>
2021-11-18 15:25:17 +0000

    created_ register_ and_ display_ on_ home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 function Home(props) {
   const navigate = useNavigate();
+  const [selectedCompetition, setSelectedCompetition] = useState("");
+  console.log({ selectedCompetition: selectedCompetition });
 
   function handleClick(event, competition) {
     event.preventDefault();
 
     navigate(`/competitions/${competition.id}`);
   }
-
-  const competitions = props.competitions.map((competition, index) => {
-    return (
-      <li key={index}>
-        <h3>{competition.exhibitionName}</h3>
-        <p>Date: {competition.date}</p>
-        <p>Location: {competition.location}</p>
-        <p>Difficulty: {competition.difficulty}</p>
-        <p>Prize Pool: {competition.prizePool}</p>
-        <p>Capacity: {competition.capacity}</p>
-        <img src={competition.img} alt="Competition" />
-        <button onClick={(event) => handleClick(event, competition)}>
-          More Details
-        </button>
-      </li>
-    );
-  });
+  const handleFilterByCompetitionLevel = (event) => {
+    setSelectedCompetition(event.target.value);
+  };
 
   return (
     <>
@@ -34,10 +23,55 @@ function Home(props) {
       <main>
         <section>
           <h2>List of all upcoming dog competitions:</h2>
-          <p>*FILTER COMPETITION BY DIFFICULTY HERE*</p>
+          <form className="">
+            <select
+              onChange={handleFilterByCompetitionLevel}
+              name="filter-by-competition"
+              id="filter-by-competition"
+              className=""
+            >
+              <option value=""> Filter Competitions By Levels</option>
+              <option value="Hard">HARD</option>
+              <option value="Medium">MEDIUM</option>
+              <option value="Easy">EASY</option>
+              {/* {props.competitions.map((comp) => (
+                <option value={comp.difficulty}>{comp.difficulty}</option>
+              ))} */}
+            </select>
+          </form>
         </section>
         <section>
-          <ul>{competitions}</ul>
+          <ul>
+            {props.competitions
+              .filter((competition) => {
+                if (
+                  selectedCompetition === competition.difficulty ||
+                  selectedCompetition === ""
+                ) {
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+              .map((competition, index) => {
+                return (
+                  <li key={index}>
+                    <h3>{competition.exhibitionName}</h3>
+                    <p>Date: {competition.date}</p>
+                    <p>Location: {competition.location}</p>
+                    <p>Difficulty: {competition.difficulty}</p>
+                    <p>Prize Pool: {competition.prizePool}</p>
+                    <p>Capacity: {competition.capacity}</p>
+                    <img src={competition.img} alt="Competition" />
+                    <button
+                      onClick={(event) => handleClick(event, competition)}
+                    >
+                      More Details
+                    </button>
+                  </li>
+                );
+              })}
+          </ul>
         </section>
       </main>
     </>
@@ -45,3 +79,4 @@ function Home(props) {
 }
 
 export default Home;
+
